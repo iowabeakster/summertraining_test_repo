3a280f70b832982b30985cadff0d936b15417675
kirk <iowabeakster@yahoo.com>
2013-08-03 14:23:48 -0400

    modularizes log_maker_script and adds another log

--- logviewer/posts/log_maker_script.py
+++ logviewer/posts/log_maker_script.py
@@ -1,16 +1,17 @@
 import json
 
+def make_new_entry():
+    n = raw_input("name:")
+    d = raw_input("date:")
+    t = raw_input("new log file to enter into the logs store:")
 
+    with open(t, "r") as f:
+        logtext_to_add = f.read()
 
-n = raw_input("name:")
-d = raw_input("date:")
-t = raw_input("new log file to enter into the logs store:")
+    new_log = json.dumps({'name': n, 'date' : d, 'text': logtext_to_add}, sort_keys = True)
 
-with open(t, "r") as f:
-    logtext_to_add = f.read()
+    with open("logs.json", "a") as myfile:
+       myfile.write(new_log)
 
-new_log = json.dumps({'name': n, 'date' : d, 'text': logtext_to_add}, sort_keys = True)
-
-with open("logs.json", "a") as myfile:
-    myfile.write(new_log)
-         
+if __name__ == '__main__':
+    make_new_entry()        
